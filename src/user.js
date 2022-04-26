2f418ed3e78fc330146538cb9b1075842622897c
Juan Ceballos <juandavi789@hotmail.com>
2022-04-25 21:30:46 -0500

    All layaout for user module

--- src/user.js
+++ src/user.js
@@ -1,10 +1,22 @@
 import { createElement } from './helperDOMfunction';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 import '../dist/style.css';
+import { content } from './index';
+import { home } from './home';
 
 function user() {
   const el = createElement('div', {
     id: 'userContainer',
-    class: ['user-container'],
+    class: ['user-container', 'final-user'],
+  });
+
+  const icon = createElement('i', {
+    class: ['fa-solid', 'fa-house', 'go-home'],
+  });
+
+  icon.addEventListener('click', () => {
+    content.innerHTML = '';
+    content.appendChild(home());
   });
 
   const para = createElement('p', {
@@ -13,6 +25,7 @@ function user() {
   });
 
   el.appendChild(para);
+  el.appendChild(icon);
 
   return el;
 }
