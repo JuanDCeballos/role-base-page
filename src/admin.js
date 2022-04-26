fc0c7043c1ee8199694d11e5daa6f0a646e5e26d
Juan Ceballos <juandavi789@hotmail.com>
2022-04-25 21:30:27 -0500

    All layaout for admin module

--- src/admin.js
+++ src/admin.js
@@ -1,10 +1,22 @@
 import { createElement } from './helperDOMfunction';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 import '../dist/style.css';
+import { content } from './index';
+import { home } from './home';
 
 function admin() {
   const el = createElement('div', {
     id: 'adminContainer',
-    class: ['admin-container'],
+    class: ['admin-container', 'final-user'],
+  });
+
+  const icon = createElement('i', {
+    class: ['fa-solid', 'fa-house', 'go-home'],
+  });
+
+  icon.addEventListener('click', () => {
+    content.innerHTML = '';
+    content.appendChild(home());
   });
 
   const para = createElement('p', {
@@ -13,6 +25,7 @@ function admin() {
   });
 
   el.appendChild(para);
+  el.appendChild(icon);
 
   return el;
 }
